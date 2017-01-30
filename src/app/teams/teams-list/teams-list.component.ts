d6043c7edb3cda8fab684873b791e3892594b185
Graham <graham.plata@gmail.com>
2017-01-30 13:59:16 -0500

    added mock team data

--- src/app/teams/teams-list/teams-list.component.ts
+++ src/app/teams/teams-list/teams-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Team } from '../shared/Team'
+import { MOCK_TEAMS } from '../shared/mock-teams'
+
 @Component({
   selector: 'app-teams-list',
   templateUrl: './teams-list.component.html',
   styleUrls: ['./teams-list.component.css']
 })
 export class TeamsListComponent implements OnInit {
+  listOfTeams = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.listOfTeams = MOCK_TEAMS;
   }
 
 }
